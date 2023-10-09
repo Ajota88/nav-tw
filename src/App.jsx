b177b2ae48dc131f7c090a1dcdf52822834b521e
Alferavil88 <alferavil88@gmail.com>
2023-10-08 23:06:30 -0500

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import Navbar from "./components/Navbar";
+
+const App = () => {
+  return (
+    <main>
+      <Navbar />
+      <div className="bg-indigo-600 w-full h-screen"></div>
+    </main>
+  );
+};
+export default App;
---
+++ src/data.js
@@ -0,0 +1,22 @@
+export const links = [
+  {
+    id: 1,
+    label: "NOSOTROS",
+    link: "#nosotros",
+  },
+  {
+    id: 2,
+    label: "SERVICIOS",
+    link: "#servicios",
+  },
+  {
+    id: 3,
+    label: "SOCIOS",
+    link: "#socios",
+  },
+  {
+    id: 4,
+    label: "CONTÁCTANOS",
+    link: "#contacto",
+  },
+];
